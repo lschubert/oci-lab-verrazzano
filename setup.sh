5d90374b1d20f21230e2937f393a7c923cc718e7
Lars Schubert <Lars.Schubert@oracle.com>
2022-08-05 13:52:01 +0000

    fix oci_cluster_id

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# for oci cmdline tool
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+# for ansible
 export ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_STDOUT_CALLBACK=debug
 ansible-playbook setup.yml
